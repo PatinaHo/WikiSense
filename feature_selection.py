605d947411eb25748d991df8074a238f9f039b5e
Patina <patina.her@gmail.com>
2020-03-13 15:11:56 +0800

    Simplify file reader

--- feature_selection.py
+++ feature_selection.py
@@ -11,16 +11,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 
 
-def get_title2GT(FILE_PATH):
-    title2GT = {}
-    
-    with open(FILE_PATH, 'r') as f:
-        for line in f:
-            title, WNsynset, refinedGT, origGT, wikiDef, WNDef = line.strip().split('\t')
-            title2GT[title] = refinedGT
-    return title2GT
-
-
 def read_categories():
     """ Reader.
     Returns:
@@ -73,58 +63,33 @@ def get_parentCategories(title, pageCat, parentCatsID, id2cat, cat2id):
     return parentsName
 
 
-def gen_features(title2GT, parentCatsID, id2cat, cat2id):
+def gen_features(FILE_PATH, parentCatsID, id2cat, cat2id):
     """
     Generate features of Wikipages. 'features' represents PE & categories.
     Arg: 
-        - title2GT(dict): {page_title: GTsynset_name}
-          ex: {'AX': 'tool.n.01', 'Aardvark': 'mammal.n.01'}
+        - title2GT(dict): {page_title: (GTsynset_name, WNaligned_synset)}
+          ex: {'Aardvark': ('mammal.n.01', 'aardvark.n.01'), 'Ceiba': ('genus.n.02', 'ceiba.n.01')}
     Return:
-        - titles(list):
-        - featuresText(list): [({'PE': ..., 'categories': [...]}, GT)]
-          ex: [({'PE': None}, 'material.n.01'),
-               ({'PE': 'fabric',
-                 'categories': ['Textiles', 'Weaving', 'Woven fabrics']}, 'fabric.n.01'),
+        - featuresText(list): [({'PE': ..., 'categories': [...]}, GT, alignWN)]
+          ex: [({'PE': 'number', 'categories': ['Ring theory', 'Elementary number theory', 'Numbers',]}, 'number.n.02', 'forty.n.01'),
                 ...]
-        - featuresVec(list): 
-        - labelVec(list): 
     """
-    
-    titles = [title for title in title2GT]
     featuresText = []
-
-    for title in title2GT:
-        parentsName = get_parentCategories(title, pageCat, parentCatsID, id2cat, cat2id)
-        PE   = re.search(r'\(.+\)', title).group()[1:-1] if re.search(r'\(.+\)', title) != None else None
-        feature = {'PE': PE, 'categories': parentsName}
-        featuresText.append((feature, title2GT[title]))
     
-    peSet  = {pageInfo[0]['PE'] for pageInfo in featuresText if pageInfo[0]['PE']!= None}
-    catSet = {cat for pageInfo in featuresText for cat in pageInfo[0]['categories']}
-    peIdx  = {pe: i for i, pe in enumerate(list(peSet))}
-    catIdx = {cat: i+len(peIdx) for i, cat in enumerate(list(catSet))}
-    
-    featuresVec = np.zeros((len(titles), len(peSet)+len(catSet)))
-    for i in range(len(titles)):
-        if featuresText[i][0]['PE']!=None:
-            pe = featuresText[i][0]['PE']
-            featuresVec[i][peIdx[pe]] = 1
-        if featuresText[i][0]['categories']!=[]:
-            for cat in featuresText[i][0]['categories']:
-                featuresVec[i][catIdx[cat]] = 1
-    
-    gtSet = {gt for title, gt in title2GT.items()}
-    gtIdx = {gt: i for i, gt in enumerate(list(gtSet))}
-    
-    labelVec = np.array([gtIdx[pageInfo[1]] for pageInfo in featuresText])
-        
-    return titles, featuresText, featuresVec, labelVec
+    with open(FILE_PATH, 'r') as f:
+        for line in f:
+            title, WNsynset, refinedGT, origGT, wikiDef, WNDef = line.strip().split('\t')
+            parentsName = get_parentCategories(title, pageCat, parentCatsID, id2cat, cat2id)
+            PE   = re.search(r'\(.+\)', title).group()[1:-1] if re.search(r'\(.+\)', title) != None else None
+            page_info = {'title': title, 'PE': PE, 'categories': parentsName, 'GT': refinedGT, 'alignSynset': WNsynset}
+            featuresText.append(page_info)
+            
+    return featuresText
 
 
 def main():
-    title2GT = get_title2GT('/home/nlplab/patina/WikiSense/data/hypernym_definition_gt.txt')
     pageCat, parentCatsID, id2cat, cat2id = read_categories()
-    titles, featuresText, featuresVec, labelVec = gen_features(title2GT, parentCatsID, id2cat, cat2id)
+    featuresText = gen_features('/home/nlplab/patina/WikiSense/data/hypernym_definition_gt.txt', parentCatsID, id2cat, cat2id)
     
 
 if __name__ == "__main__":
