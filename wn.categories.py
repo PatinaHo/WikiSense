ff91f96679b4aa01f43914140b2dacc56bc0450c
Patina <patina.her@gmail.com>
2020-03-25 15:56:01 +0800

    Add function docs.

--- wn.categories.py
+++ wn.categories.py
@@ -4,6 +4,10 @@
 import sys
 from nltk.corpus import wordnet as wn
 
+"""
+catMembers = {catSynset1: (catNumber, descendents), catSynset2: (catNumber, descendents), ...}
+cat        = {synset: catNumber}
+"""
 catMembers = {}
 cat = {}
 
@@ -13,6 +17,11 @@ catNumber = 0
 fileout = open('wn.500.categories.txt', 'w')
 
 def unregistered_descendents(syn):
+    """
+    Return syn itself and its recurring unregistered descendents, if syn is unregistered (isn't initially in cat).
+    Return:
+        res: [descendent_synset1, descendent_synset2, ...]
+    """
     if syn in cat:
         return []
 
@@ -22,12 +31,21 @@ def unregistered_descendents(syn):
     return res
 
 def mark(syn, descendents, catNumber):
+    """
+    Changes catMembers and cat.
+    Returns nothing.
+    """
     catMembers[syn] = (catNumber, descendents)
     for d in descendents:
         cat[d] = catNumber
     return
 
-def addCategories(n):
+def add_categories(n):
+    """
+    Classify synsets with descendents number in range(LOWER, UPPER) as a category.
+    Changes catMembers and cat.
+    Returns nothing.
+    """
     global catNumber
     if n in catMembers: return
 
@@ -44,7 +62,7 @@ def addCategories(n):
                 catNumber += 1
                 mark(hypo, descendents, catNumber)
             elif total > UPPER:
-                addCategories(hypo)
+                add_categories(hypo)
 
         descendents = unregistered_descendents(n)
         total = len(descendents)
@@ -53,19 +71,23 @@ def addCategories(n):
             catNumber += 1
             mark(n, descendents, catNumber)
 
-#nouns = [ wn.synsets('plant', 'n')[1] ]
-nouns = [ s for s in wn.all_synsets() if s.pos == 'n']
 
+def 
+
+nouns = [ s for s in wn.all_synsets() if s.pos == 'n']
 for n in nouns:
-  addCategories(n)
-  
+  add_categories(n)
+
+"""
+cats = [(catNumber, catsynset, [descendentSyn1, descendentSyn2, ...])]
+"""
 cats = [ (catMembers[syn][0], syn, catMembers[syn][1]) for syn in catMembers.keys()]
 cats.sort(key = lambda x: x[0])
 
 for no, syn, members in cats:
-  print(fileout, no, syn.name)
+    print(fileout, no, syn.name)
 
-  y = [ ', '.join([ x for x in m.lemma_names if '_' not in x and '-' not in x] ) for m in members ]
-  print(fileout, '; '.join( [ z for z in y if z != '' ] ))
-  print(fileout)
+    y = [ ', '.join([ x for x in m.lemma_names if '_' not in x and '-' not in x] ) for m in members ]
+    print(fileout, '; '.join( [ z for z in y if z != '' ] ))
+    print(fileout)
 fileout.close()
